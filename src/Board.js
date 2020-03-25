b4c4e0ab011784addcfab90e9f7764ecd96627d2
Soslan <sparksoslan@gmail.com>
2020-03-25 21:11:32 +0000

    Basic layout of board and cells Changed board array values to booleans instead of strings

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Cell from './Cell';
+import './Board.css';
 
 /** Game Board
  *
@@ -17,13 +18,13 @@ class Board extends Component {
 
   state = {
     hasWon: false,
-    board: []
+    board: 0
   };
 
   createBoard = () => {
     const newBoard = Array(this.props.nRows)
-      .fill('.')
-      .map(() => Array(this.props.nCols).fill('.'));
+      .fill(false)
+      .map(() => Array(this.props.nCols).fill(false));
     console.log(newBoard);
     return this.setState({
       hasWon: false,
@@ -31,12 +32,29 @@ class Board extends Component {
     });
   };
 
+  // TODO: Handle changing cells and update the board
+
+  // Function to check coordinate of click cell and determine if the cell is on of off
+  // Turn on/off surround cells
+  // Check if user has won by determining if all cells are off
+
   render() {
     return (
       <div>
         <button onClick={this.createBoard}> rows</button>
         <h1>{this.props.nCols} Cols</h1>
-        <Cell />
+
+        {/* Game board should look like this */}
+        <table>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
--- src/Cell.js
+++ src/Cell.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import './Cell.css';
 
 class Cell extends Component {
+  // Listen to click and handle the event
+  handleClick = e => {};
+
   render() {
-    return (
-      <div>
-        <h1>I'm the cell</h1>
-      </div>
-    );
+    return <td onClick={this.handleClick} />;
   }
 }
 
